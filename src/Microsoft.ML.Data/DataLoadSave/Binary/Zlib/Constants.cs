c6c8b27d3165d60fb662db817ed0492c79f30b7e
CMSQATeam <cmsqa123@gmail.com>
2019-01-30 14:36:16 +0530

    DW CMS Message

--- src/Microsoft.ML.Data/DataLoadSave/Binary/Zlib/Constants.cs
+++ src/Microsoft.ML.Data/DataLoadSave/Binary/Zlib/Constants.cs
@@ -84,47 +84,62 @@ namespace Microsoft.ML.Data.IO.Zlib
 
         
         
+        
         public enum Level
         {
+            
             
             
             DefaultCompression = -1,
             
             
+            
             Level0 = 0,
+            
             
             
             NoCompression = 0,
             
             
+            
             BestSpeed = 1,
+            
             
             
             Level1 = 1,
             
             
+            
             Level2 = 2,
+            
             
             
             Level3 = 3,
             
             
+            
             Level4 = 4,
+            
             
             
             Level5 = 5,
             
             
+            
             Level6 = 6,
+            
             
             
             Level7 = 7,
             
             
+            
             Level8 = 8,
             
             
+            
             BestCompression = 9,
+            
             
             
             Level9 = 9,
