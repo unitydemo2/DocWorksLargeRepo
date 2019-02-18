f86e83ddfb4186f8a4a918aeab4b5c3580718fb9
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 19:22:52 +0530

    DW CMS Message

--- src/Microsoft.ML.FastTree/FastTreeRegression.cs
+++ src/Microsoft.ML.FastTree/FastTreeRegression.cs
@@ -440,6 +440,7 @@ namespace Microsoft.ML.Trainers.FastTree
         }
     }
 
+    
     public sealed class FastTreeRegressionModelParameters : TreeEnsembleModelParameters
     {
         internal const string LoaderSignature = "FastTreeRegressionExec";
@@ -460,12 +461,16 @@ namespace Microsoft.ML.Trainers.FastTree
                 loaderAssemblyName: typeof(FastTreeRegressionModelParameters).Assembly.FullName);
         }
 
+        
         protected override uint VerNumFeaturesSerialized => 0x00010002;
 
+        
         protected override uint VerDefaultValueSerialized => 0x00010004;
 
+        
         protected override uint VerCategoricalSplitSerialized => 0x00010005;
 
+        
         public FastTreeRegressionModelParameters(IHostEnvironment env, TreeEnsemble trainedEnsemble, int featureCount, string innerArgs)
             : base(env, RegistrationName, trainedEnsemble, featureCount, innerArgs)
         {
@@ -476,6 +481,7 @@ namespace Microsoft.ML.Trainers.FastTree
         {
         }
 
+        
         private protected override void SaveCore(ModelSaveContext ctx)
         {
             base.SaveCore(ctx);
@@ -490,6 +496,7 @@ namespace Microsoft.ML.Trainers.FastTree
             return new FastTreeRegressionModelParameters(env, ctx);
         }
 
+        
         public override PredictionKind PredictionKind => PredictionKind.Regression;
     }
 
