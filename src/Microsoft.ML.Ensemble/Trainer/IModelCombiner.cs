ac7576e223c5a061544612474f93a6fcf6335472
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 13:26:32 +0530

    DW CMS Message

--- src/Microsoft.ML.Ensemble/Trainer/IModelCombiner.cs
+++ src/Microsoft.ML.Ensemble/Trainer/IModelCombiner.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -8,11 +8,12 @@ namespace Microsoft.ML.Ensemble
 {
     public delegate void SignatureModelCombiner(PredictionKind kind);
 
-    /// <summary>
-    /// An interface that combines multiple predictors into a single predictor.
-    /// </summary>
-    public interface IModelCombiner
+    ///     <summary>
+        ///     An interface that combines multiple predictors into a single predictor.
+        ///     </summary>
+            public interface IModelCombiner
     {
+        
         IPredictor CombineModels(IEnumerable<IPredictor> models);
     }
 }
