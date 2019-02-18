b4cce940c37def43977d919757570a9017257c6f
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 21:56:07 +0530

    DW CMS Message

--- src/Microsoft.ML.TensorFlow/TensorFlowModelInfo.cs
+++ src/Microsoft.ML.TensorFlow/TensorFlowModelInfo.cs
@@ -7,21 +7,11 @@ using Microsoft.ML.Transforms.TensorFlow;
 
 namespace Microsoft.ML.Transforms
 {
-    /// <summary>
-    /// This class holds the information related to TensorFlow model and session.
-    /// It provides a convenient way to query model schema as follows.
-    /// <list type="bullet">
-    ///    <item>
-    ///      <description>Get complete schema by calling <see cref="GetModelSchema()"/>.</description>
-    ///    </item>
-    ///    <item>
-    ///      <description>Get schema related to model input(s) by calling <see cref="GetInputSchema()"/>.</description>
-    ///    </item>
-    /// </list>
-    /// </summary>
-    public class TensorFlowModelInfo
+    /// <!-- Badly formed XML comment ignored for member "T:Microsoft.ML.Transforms.TensorFlowModelInfo" -->
+            public class TensorFlowModelInfo
     {
         internal TFSession Session { get; }
+        
         public string ModelPath { get; }
 
         private readonly IHostEnvironment _env;
@@ -47,11 +37,11 @@ namespace Microsoft.ML.Transforms
             return TensorFlowUtils.GetModelSchema(_env, Session.Graph);
         }
 
-        /// <summary>
-        /// Get <see cref="ISchema"/> for only those nodes which are marked "Placeholder" in the TensorFlow model.
-        /// This method is convenient for exploring the model input(s) in case TensorFlow graph is very large.
-        /// </summary>
-        public Schema GetInputSchema()
+        ///     <summary>
+                ///     Get <see cref="ISchema"/> for only those nodes which are marked "Placeholder" in the TensorFlow model.
+                ///     This method is convenient for exploring the model input(s) in case TensorFlow graph is very large.
+                ///     </summary>
+                        public Schema GetInputSchema()
         {
             return TensorFlowUtils.GetModelSchema(_env, Session.Graph, "Placeholder");
         }
