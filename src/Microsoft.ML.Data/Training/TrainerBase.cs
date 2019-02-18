b5d26a39cb39a8546e9117155384954ef0fb8dfa
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 19:55:36 +0530

    DW CMS Message

--- src/Microsoft.ML.Data/Training/TrainerBase.cs
+++ src/Microsoft.ML.Data/Training/TrainerBase.cs
@@ -4,21 +4,27 @@
 
 namespace Microsoft.ML.Training
 {
+    
     public abstract class TrainerBase<TPredictor> : ITrainer<TPredictor>
         where TPredictor : IPredictor
     {
-        /// <summary>
-        /// A standard string to use in errors or warnings by subclasses, to communicate the idea that no valid
-        /// instances were able to be found.
-        /// </summary>
-        protected const string NoTrainingInstancesMessage = "No valid training instances found, all instances have missing features.";
+        ///     <summary>
+                ///     A standard string to use in errors or warnings by subclasses, to communicate the idea that no valid
+                ///     instances were able to be found.
+                ///     </summary>
+                        protected const string NoTrainingInstancesMessage = "No valid training instances found, all instances have missing features.";
 
+        
         protected IHost Host { get; }
 
+        
         public string Name { get; }
+        
         public abstract PredictionKind PredictionKind { get; }
+        
         public abstract TrainerInfo Info { get; }
 
+        
         [BestFriend]
         private protected TrainerBase(IHostEnvironment env, string name)
         {
@@ -29,10 +35,13 @@ namespace Microsoft.ML.Training
             Host = env.Register(name);
         }
 
+        
         IPredictor ITrainer.Train(TrainContext context) => Train(context);
 
+        
         TPredictor ITrainer<TPredictor>.Train(TrainContext context) => Train(context);
 
+        
         [BestFriend]
         private protected abstract TPredictor Train(TrainContext context);
     }
