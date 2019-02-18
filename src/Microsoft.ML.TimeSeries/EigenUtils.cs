4304151f176fc5747635deff132c4a07edebdf16
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 18:19:21 +0530

    DW CMS Message

--- src/Microsoft.ML.TimeSeries/EigenUtils.cs
+++ src/Microsoft.ML.TimeSeries/EigenUtils.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -457,10 +457,14 @@ namespace Microsoft.ML.TimeSeriesProcessing
             Schur = (byte)'S'
         }
 
+        
         public enum Compz : byte
         {
+            
             None = (byte)'N',
+            
             SchurH = (byte)'I',
+            
             SchurA = (byte)'V'
         }
 
