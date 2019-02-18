93f0e87f61d58884fcbc04141402202ce4458a77
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 13:23:56 +0530

    DW CMS Message

--- src/Microsoft.ML.Ensemble/Selector/ISubsetSelector.cs
+++ src/Microsoft.ML.Ensemble/Selector/ISubsetSelector.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -17,6 +17,7 @@ namespace Microsoft.ML.Ensemble.Selector
         RoleMappedData GetTestData(Subset subset, Batch batch);
     }
 
+    
     public delegate void SignatureEnsembleDataSelector();
 
     [TlcModule.ComponentKind("EnsembleSubsetSelector")]
