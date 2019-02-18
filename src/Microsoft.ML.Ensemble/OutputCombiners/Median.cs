c4f2e1441f8b2313a2cbfe13b00ba428bbe454bb
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 13:12:32 +0530

    DW CMS Message

--- src/Microsoft.ML.Ensemble/OutputCombiners/Median.cs
+++ src/Microsoft.ML.Ensemble/OutputCombiners/Median.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -13,14 +13,17 @@ using Microsoft.ML.Model;
 
 namespace Microsoft.ML.Ensemble.OutputCombiners
 {
-    /// <summary>
-    /// Generic interface for combining outputs of multiple models
-    /// </summary>
-    public sealed class Median : IRegressionOutputCombiner, IBinaryOutputCombiner, ICanSaveModel
+    ///     <summary>
+        ///     Generic interface for combining outputs of multiple models
+        ///     </summary>
+            public sealed class Median : IRegressionOutputCombiner, IBinaryOutputCombiner, ICanSaveModel
     {
         private readonly IHost _host;
+        
         public const string UserName = "Median";
+        
         public const string LoadName = "Median";
+        
         public const string LoaderSignature = "MedianCombiner";
 
         private static VersionInfo GetVersionInfo()
@@ -34,6 +37,7 @@ namespace Microsoft.ML.Ensemble.OutputCombiners
                 loaderAssemblyName: typeof(Median).Assembly.FullName);
         }
 
+        
         public Median(IHostEnvironment env)
         {
             Contracts.CheckValue(env, nameof(env));
@@ -51,6 +55,7 @@ namespace Microsoft.ML.Ensemble.OutputCombiners
             _host.CheckDecode(cbFloat == sizeof(Single));
         }
 
+        
         public static Median Create(IHostEnvironment env, ModelLoadContext ctx)
         {
             Contracts.CheckValue(env, nameof(env));
@@ -59,6 +64,7 @@ namespace Microsoft.ML.Ensemble.OutputCombiners
             return new Median(env, ctx);
         }
 
+        
         public void Save(ModelSaveContext ctx)
         {
             _host.CheckValue(ctx, nameof(ctx));
@@ -70,6 +76,7 @@ namespace Microsoft.ML.Ensemble.OutputCombiners
             ctx.Writer.Write(sizeof(Single));
         }
 
+        
         public Combiner<Single> GetCombiner()
         {
             return CombineCore;
