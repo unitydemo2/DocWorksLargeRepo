5a312a35d18657d7966f712aa1635089776874f3
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 11:48:48 +0530

    DW CMS Message

--- src/Microsoft.ML.Data/DataLoadSave/Text/LoadColumnAttribute.cs
+++ src/Microsoft.ML.Data/DataLoadSave/Text/LoadColumnAttribute.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -10,18 +10,18 @@ namespace Microsoft.ML.Data
     // REVIEW: The Start field is decorated with [Obsolete], and this warning disables using Obsolete for this class.
     // The Start field should get deleted together with the Legacy API.
 #pragma warning disable 618
-    /// <summary>
-    /// Describes column information such as name and the source columns indices that this
-    /// column encapsulates.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    ///     <summary>
+    ///     Describes column information such as name and the source columns indices that this
+    ///     column encapsulates.
+    ///     </summary>
+        [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
     public sealed class LoadColumnAttribute : Attribute
     {
-        /// <summary>
-        /// Initializes new instance of <see cref="LoadColumnAttribute"/>.
-        /// </summary>
-        /// <param name="columnIndex">The index of the column in the text file.</param>
-        // REVIEW: Remove calling the private constructor with just the start parameter,
+        ///     <summary>
+        ///     Initializes new instance of <see cref="LoadColumnAttribute"/>.
+        ///     </summary>
+        ///     <param name="columnIndex">The index of the column in the text file.</param>
+                // REVIEW: Remove calling the private constructor with just the start parameter,
         // when the Legacy API's TextLoader gets deleted, and with it the Start field here.
         public LoadColumnAttribute(int columnIndex)
              : this(columnIndex.ToString())
@@ -29,12 +29,12 @@ namespace Microsoft.ML.Data
             Sources.Add(new TextLoader.Range(columnIndex));
         }
 
-        /// <summary>
-        /// Initializes new instance of <see cref="LoadColumnAttribute"/>.
-        /// </summary>
-        /// <param name="start">The starting column index, for the range.</param>
-        /// <param name="end">The ending column index, for the range.</param>
-        // REVIEW: Calling the private constructor with just the start parameter, is incorrect,
+        ///     <summary>
+        ///     Initializes new instance of <see cref="LoadColumnAttribute"/>.
+        ///     </summary>
+        ///     <param name="start">The starting column index, for the range.</param>
+        ///     <param name="end">The ending column index, for the range.</param>
+                // REVIEW: Calling the private constructor with just the start parameter, is incorrect,
         // but it is just temporary there, until the Legacy API's TextLoader gets deleted, together with the Start field.
         public LoadColumnAttribute(int start, int end)
              : this(start.ToString())
@@ -42,11 +42,11 @@ namespace Microsoft.ML.Data
             Sources.Add(new TextLoader.Range(start, end));
         }
 
-        /// <summary>
-        /// Initializes new instance of <see cref="LoadColumnAttribute"/>.
-        /// </summary>
-        /// <param name="columnIndexes">Distinct text file column indices to load as part of this column.</param>
-        // REVIEW: Calling the private constructor with just the columnIndexes[0] parameter, is incorrect,
+        ///     <summary>
+        ///     Initializes new instance of <see cref="LoadColumnAttribute"/>.
+        ///     </summary>
+        ///     <param name="columnIndexes">Distinct text file column indices to load as part of this column.</param>
+                // REVIEW: Calling the private constructor with just the columnIndexes[0] parameter, is incorrect,
         // but it is just temporary there, until the Legacy API's TextLoader gets deleted together with the Start field.
         public LoadColumnAttribute(int[] columnIndexes)
             : this(columnIndexes[0].ToString()) // REVIEW: this is incorrect, but it is just temporary there, until the Legacy API's TextLoader gets deleted.
