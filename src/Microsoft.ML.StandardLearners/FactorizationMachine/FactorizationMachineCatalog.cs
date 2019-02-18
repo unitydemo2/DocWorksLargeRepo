ec6ec467440c2bbe242db0d0a237ff1b417651f3
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 22:33:34 +0530

    DW CMS Message

--- src/Microsoft.ML.StandardLearners/FactorizationMachine/FactorizationMachineCatalog.cs
+++ src/Microsoft.ML.StandardLearners/FactorizationMachine/FactorizationMachineCatalog.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -8,23 +8,23 @@ using Microsoft.ML.FactorizationMachine;
 
 namespace Microsoft.ML
 {
-    /// <summary>
-    /// Extension method to create <see cref="FieldAwareFactorizationMachineTrainer"/>
-    /// </summary>
-    public static class FactorizationMachineExtensions
+    ///     <summary>
+        ///     Extension method to create <see cref="FieldAwareFactorizationMachineTrainer"/>
+        ///     </summary>
+            public static class FactorizationMachineExtensions
     {
-        /// <summary>
-        /// Predict a target using a field-aware factorization machine algorithm.
-        /// </summary>
-        /// <param name="ctx">The binary classification context trainer object.</param>
-        /// <param name="featureColumns">The features, or independent variables.</param>
-        /// <param name="labelColumn">The label, or dependent variable.</param>
-        /// <param name="weights">The optional example weights.</param>
-        /// <param name="advancedSettings">A delegate to set more settings.
-        /// The settings here will override the ones provided in the direct method signature,
-        /// if both are present and have different values.
-        /// The columns names, however need to be provided directly, not through the <paramref name="advancedSettings"/>.</param>
-        public static FieldAwareFactorizationMachineTrainer FieldAwareFactorizationMachine(this BinaryClassificationContext.BinaryClassificationTrainers ctx,
+        ///     <summary>
+                ///     Predict a target using a field-aware factorization machine algorithm.
+                ///     </summary>
+                ///     <param name="ctx">The binary classification context trainer object.</param>
+                ///     <param name="featureColumns">The features, or independent variables.</param>
+                ///     <param name="labelColumn">The label, or dependent variable.</param>
+                ///     <param name="weights">The optional example weights.</param>
+                ///     <param name="advancedSettings">A delegate to set more settings.
+                ///     The settings here will override the ones provided in the direct method signature,
+                ///     if both are present and have different values.
+                ///     The columns names, however need to be provided directly, not through the <paramref name="advancedSettings"/>.</param>
+                        public static FieldAwareFactorizationMachineTrainer FieldAwareFactorizationMachine(this BinaryClassificationContext.BinaryClassificationTrainers ctx,
             string[] featureColumns,
             string labelColumn = DefaultColumnNames.Label,
             string weights = null,
