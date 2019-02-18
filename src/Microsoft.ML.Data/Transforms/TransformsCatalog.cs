31b3ae7622f324a722c2c4a2bef86e49757135ed
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 23:23:18 +0530

    DW CMS Message

--- src/Microsoft.ML.Data/Transforms/TransformsCatalog.cs
+++ src/Microsoft.ML.Data/Transforms/TransformsCatalog.cs
@@ -79,11 +79,12 @@ namespace Microsoft.ML
             }
         }
 
-        /// <summary>
-        /// The catalog of projection operations.
-        /// </summary>
-        public sealed class ProjectionTransforms : SubCatalogBase
+        ///     <summary>
+                ///     The catalog of projection operations.
+                ///     </summary>
+                        public sealed class ProjectionTransforms : SubCatalogBase
         {
+            
             public ProjectionTransforms(TransformsCatalog owner) : base(owner)
             {
             }
