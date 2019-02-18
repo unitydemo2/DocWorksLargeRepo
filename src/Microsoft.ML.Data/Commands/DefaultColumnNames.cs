19d492ce4b3af14c5a79711812cf17a131f98724
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 11:40:33 +0530

    DW CMS Message

--- src/Microsoft.ML.Data/Commands/DefaultColumnNames.cs
+++ src/Microsoft.ML.Data/Commands/DefaultColumnNames.cs
@@ -4,20 +4,34 @@
 
 namespace Microsoft.ML.Data
 {
+    
     public static class DefaultColumnNames
     {
+        
         public const string Features = "Features";
+        
         public const string Label = "Label";
+        
         public const string GroupId = "GroupId";
+        
         public const string Name = "Name";
+        
         public const string Weight = "Weight";
+        
         public const string Score = "Score";
+        
         public const string Probability = "Probability";
+        
         public const string PredictedLabel = "PredictedLabel";
+        
         public const string RecommendedItems = "Recommended";
+        
         public const string User = "User";
+        
         public const string Item = "Item";
+        
         public const string Date = "Date";
+        
         public const string FeatureContributions = "FeatureContributions";
     }
 }
