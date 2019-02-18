a3dbfe4e752f92a6ce39d539eb48a905a5af5826
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 11:27:28 +0530

    DW CMS Message

--- src/Microsoft.ML.Data/Evaluators/Metrics/ClusteringMetrics.cs
+++ src/Microsoft.ML.Data/Evaluators/Metrics/ClusteringMetrics.cs
@@ -4,33 +4,25 @@
 
 namespace Microsoft.ML.Data
 {
-    /// <summary>
-    /// The metrics generated after evaluating the clustering predictions.
-    /// </summary>
-    public sealed class ClusteringMetrics
+    ///     <summary>
+        ///     The metrics generated after evaluating the clustering predictions.
+        ///     </summary>
+            public sealed class ClusteringMetrics
     {
-        /// <summary>
-        /// Normalized Mutual Information
-        /// NMI is a measure of the mutual dependence of the variables.
-        /// <a href="http://en.wikipedia.org/wiki/Mutual_information#Normalized_variants">Normalized variants</a> work on data that already has cluster labels.
-        /// Its value ranged from 0 to 1, where higher numbers are better.
-        /// </summary>
-        public double Nmi { get; }
+        /// <!-- Badly formed XML comment ignored for member "P:Microsoft.ML.Data.ClusteringMetrics.Nmi" -->
+                        public double Nmi { get; }
 
-        /// <summary>
-        /// Average Score. For the K-Means algorithm, the &apos;score&apos; is the distance from the centroid to the example.
-        /// The average score is, therefore, a measure of proximity of the examples to cluster centroids.
-        /// In other words, it&apos;s the &apos;cluster tightness&apos; measure.
-        /// Note however, that this metric will only decrease if the number of clusters is increased,
-        /// and in the extreme case (where each distinct example is its own cluster) it will be equal to zero.
-        /// </summary>
-        public double AvgMinScore { get; }
+        ///     <summary>
+                ///     Average Score. For the K-Means algorithm, the 'score' is the distance from the centroid to the example.
+                ///     The average score is, therefore, a measure of proximity of the examples to cluster centroids.
+                ///     In other words, it's the 'cluster tightness' measure.
+                ///     Note however, that this metric will only decrease if the number of clusters is increased,
+                ///     and in the extreme case (where each distinct example is its own cluster) it will be equal to zero.
+                ///     </summary>
+                        public double AvgMinScore { get; }
 
-        /// <summary>
-        /// <a href="https://en.wikipedia.org/wiki/Davies%E2%80%93Bouldin_index">Davies-Bouldin Index</a>
-        /// DBI is a measure of the how much scatter is in the cluster and the cluster separation.
-        /// </summary>
-        public double Dbi { get; }
+        /// <!-- Badly formed XML comment ignored for member "P:Microsoft.ML.Data.ClusteringMetrics.Dbi" -->
+                        public double Dbi { get; }
 
         internal ClusteringMetrics(IExceptionContext ectx, Row overallResult, bool calculateDbi)
         {
