cae3602079ec7c699724091b0e2f61282eba61ed
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 19:57:05 +0530

    DW CMS Message

--- src/Microsoft.ML.Data/Training/ITrainerEstimator.cs
+++ src/Microsoft.ML.Data/Training/ITrainerEstimator.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -6,12 +6,15 @@ using Microsoft.ML.Core.Data;
 
 namespace Microsoft.ML.Training
 {
+    
     public interface ITrainerEstimator<out TTransformer, out TPredictor> : IEstimator<TTransformer>
         where TTransformer : ISingleFeaturePredictionTransformer<TPredictor>
         where TPredictor : IPredictor
     {
+        
         TrainerInfo Info { get; }
 
+        
         PredictionKind PredictionKind { get; }
     }
 }
