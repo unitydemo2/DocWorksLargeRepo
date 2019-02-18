a6f63cd0d6954db44c39c01a9bf4ab1d80f1657d
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 14:01:39 +0530

    DW CMS Message

--- src/Microsoft.ML.EntryPoints/MacroUtils.cs
+++ src/Microsoft.ML.EntryPoints/MacroUtils.cs
@@ -17,18 +17,25 @@ namespace Microsoft.ML.EntryPoints
     
     public static class MacroUtils
     {
-        /// <summary>
-        /// Lists the types of trainer signatures. Used by entry points and autoML system
-        /// to know what types of evaluators to use for the train test / pipeline sweeper.
-        /// </summary>
-        public enum TrainerKinds
+        ///     <summary>
+                ///     Lists the types of trainer signatures. Used by entry points and autoML system
+                ///     to know what types of evaluators to use for the train test / pipeline sweeper.
+                ///     </summary>
+                        public enum TrainerKinds
         {
+            
             SignatureBinaryClassifierTrainer,
+            
             SignatureMultiClassClassifierTrainer,
+            
             SignatureRankerTrainer,
+            
             SignatureRegressorTrainer,
+            
             SignatureMultiOutputRegressorTrainer,
+            
             SignatureAnomalyDetectorTrainer,
+            
             SignatureClusteringTrainer,
         }
 
