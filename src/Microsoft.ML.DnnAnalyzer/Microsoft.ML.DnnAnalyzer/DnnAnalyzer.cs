dec37fa95b56c3a770a56799e9223c828808c21c
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 13:06:51 +0530

    DW CMS Message

--- src/Microsoft.ML.DnnAnalyzer/Microsoft.ML.DnnAnalyzer/DnnAnalyzer.cs
+++ src/Microsoft.ML.DnnAnalyzer/Microsoft.ML.DnnAnalyzer/DnnAnalyzer.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -7,8 +7,10 @@ using Microsoft.ML.Transforms.TensorFlow;
 
 namespace Microsoft.ML.DnnAnalyzer
 {
+    
     public static class DnnAnalyzer
     {
+        
         public static void Main(string[] args)
         {
             if (args == null || args.Length != 1)
