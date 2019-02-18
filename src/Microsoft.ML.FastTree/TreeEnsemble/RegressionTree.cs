d5e5539d0fd0cf7133429881f750ed0ae9124667
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 19:01:39 +0530

    DW CMS Message

--- src/Microsoft.ML.FastTree/TreeEnsemble/RegressionTree.cs
+++ src/Microsoft.ML.FastTree/TreeEnsemble/RegressionTree.cs
@@ -59,15 +59,18 @@ namespace Microsoft.ML.Trainers.FastTree.Internal
         public double[] PreviousLeafValues { get { return _previousLeafValue; } }
         public double[] LeafValues { get; }
 
-        /// <summary>
-        /// Code to identify the type of tree in binary serialization. These values are
-        /// persisted, so they should remain consistent for the sake of deserialization
-        /// backwards compatibility.
-        /// </summary>
-        protected enum TreeType : byte
+        ///     <summary>
+                ///     Code to identify the type of tree in binary serialization. These values are
+                ///     persisted, so they should remain consistent for the sake of deserialization
+                ///     backwards compatibility.
+                ///     </summary>
+                        protected enum TreeType : byte
         {
+            
             Regression = 0,
+            
             Affine = 1,
+            
             FastForest = 2
         }
 
