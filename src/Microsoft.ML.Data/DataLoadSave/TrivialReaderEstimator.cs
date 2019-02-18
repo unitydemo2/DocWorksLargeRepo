0ce4fe49a72bea14f6bc15007227a9fa8c01687e
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 12:53:59 +0530

    DW CMS Message

--- src/Microsoft.ML.Data/DataLoadSave/TrivialReaderEstimator.cs
+++ src/Microsoft.ML.Data/DataLoadSave/TrivialReaderEstimator.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -6,21 +6,25 @@ using Microsoft.ML.Core.Data;
 
 namespace Microsoft.ML.Data
 {
-    /// <summary>
-    /// The trivial wrapper for a <see cref="IDataReader{TSource}"/> that acts as an estimator and ignores the source.
-    /// </summary>
-    public sealed class TrivialReaderEstimator<TSource, TReader>: IDataReaderEstimator<TSource, TReader>
+    ///     <summary>
+        ///     The trivial wrapper for a <see cref="IDataReader{TSource}"/> that acts as an estimator and ignores the source.
+        ///     </summary>
+            public sealed class TrivialReaderEstimator<TSource, TReader>: IDataReaderEstimator<TSource, TReader>
         where TReader: IDataReader<TSource>
     {
+        
         public TReader Reader { get; }
 
+        
         public TrivialReaderEstimator(TReader reader)
         {
             Reader = reader;
         }
 
+        
         public TReader Fit(TSource input) => Reader;
 
+        
         public SchemaShape GetOutputSchema() => SchemaShape.Create(Reader.GetOutputSchema());
     }
 }
