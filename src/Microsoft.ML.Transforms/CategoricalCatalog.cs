d9ac4ae7ed18b0279e3e0173b6c9fb00c2854ad3
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 22:19:01 +0530

    DW CMS Message

--- src/Microsoft.ML.Transforms/CategoricalCatalog.cs
+++ src/Microsoft.ML.Transforms/CategoricalCatalog.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -7,49 +7,30 @@ using Microsoft.ML.Transforms.Categorical;
 
 namespace Microsoft.ML
 {
-    /// <summary>
-    /// Static extensions for categorical transforms.
-    /// </summary>
-    public static class CategoricalCatalog
+    ///     <summary>
+        ///     Static extensions for categorical transforms.
+        ///     </summary>
+            public static class CategoricalCatalog
     {
-        /// <summary>
-        /// Convert a text column into one-hot encoded vector.
-        /// </summary>
-        /// <param name="catalog">The transform catalog</param>
-        /// <param name="inputColumn">The input column</param>
-        /// <param name="outputColumn">The output column. If <c>null</c>, <paramref name="inputColumn"/> is used.</param>
-        /// <param name="outputKind">The conversion mode.</param>
-        /// <returns></returns>
-        public static OneHotEncodingEstimator OneHotEncoding(this TransformsCatalog.CategoricalTransforms catalog,
+        /// <!-- Badly formed XML comment ignored for member "M:Microsoft.ML.CategoricalCatalog.OneHotEncoding(Microsoft.ML.TransformsCatalog.CategoricalTransforms,System.String,System.String,Microsoft.ML.Transforms.Categorical.OneHotEncodingTransformer.OutputKind)" -->
+                        public static OneHotEncodingEstimator OneHotEncoding(this TransformsCatalog.CategoricalTransforms catalog,
                 string inputColumn,
                 string outputColumn = null,
                 OneHotEncodingTransformer.OutputKind outputKind = OneHotEncodingTransformer.OutputKind.Ind)
             => new OneHotEncodingEstimator(CatalogUtils.GetEnvironment(catalog), inputColumn, outputColumn, outputKind);
 
-        /// <summary>
-        /// Convert several text column into one-hot encoded vectors.
-        /// </summary>
-        /// <param name="catalog">The transform catalog</param>
-        /// <param name="columns">The column settings.</param>
-        /// <returns></returns>
-        public static OneHotEncodingEstimator OneHotEncoding(this TransformsCatalog.CategoricalTransforms catalog,
+        ///     <summary>
+                ///     Convert several text column into one-hot encoded vectors.
+                ///     </summary>
+                ///     <param name="catalog">The transform catalog</param>
+                ///     <param name="columns">The column settings.</param>
+                ///     <returns></returns>
+                        public static OneHotEncodingEstimator OneHotEncoding(this TransformsCatalog.CategoricalTransforms catalog,
                 params OneHotEncodingEstimator.ColumnInfo[] columns)
             => new OneHotEncodingEstimator(CatalogUtils.GetEnvironment(catalog), columns);
 
-        /// <summary>
-        /// Convert a text column into hash-based one-hot encoded vector.
-        /// </summary>
-        /// <param name="catalog">The transform catalog</param>
-        /// <param name="inputColumn">The input column</param>
-        /// <param name="outputColumn">The output column. If <c>null</c>, <paramref name="inputColumn"/> is used.</param>
-        /// <param name="hashBits">Number of bits to hash into. Must be between 1 and 30, inclusive.</param>
-        /// <param name="invertHash">During hashing we constuct mappings between original values and the produced hash values.
-        /// Text representation of original values are stored in the slot names of the  metadata for the new column.Hashing, as such, can map many initial values to one.
-        /// <paramref name="invertHash"/> specifies the upper bound of the number of distinct input values mapping to a hash that should be retained.
-        /// <value>0</value> does not retain any input values. <value>-1</value> retains all input values mapping to each hash.</param>
-        /// <param name="outputKind">The conversion mode.</param>
-        /// <returns></returns>
-        public static OneHotHashEncodingEstimator OneHotHashEncoding(this TransformsCatalog.CategoricalTransforms catalog,
+        /// <!-- Badly formed XML comment ignored for member "M:Microsoft.ML.CategoricalCatalog.OneHotHashEncoding(Microsoft.ML.TransformsCatalog.CategoricalTransforms,System.String,System.String,System.Int32,System.Int32,Microsoft.ML.Transforms.Categorical.OneHotEncodingTransformer.OutputKind)" -->
+                        public static OneHotHashEncodingEstimator OneHotHashEncoding(this TransformsCatalog.CategoricalTransforms catalog,
                 string inputColumn,
                 string outputColumn = null,
                 int hashBits = OneHotHashEncodingEstimator.Defaults.HashBits,
@@ -57,13 +38,13 @@ namespace Microsoft.ML
                 OneHotEncodingTransformer.OutputKind outputKind = OneHotEncodingTransformer.OutputKind.Ind)
             => new OneHotHashEncodingEstimator(CatalogUtils.GetEnvironment(catalog), inputColumn, outputColumn, hashBits, invertHash, outputKind);
 
-        /// <summary>
-        /// Convert several text column into hash-based one-hot encoded vectors.
-        /// </summary>
-        /// <param name="catalog">The transform catalog</param>
-        /// <param name="columns">The column settings.</param>
-        /// <returns></returns>
-        public static OneHotHashEncodingEstimator OneHotHashEncoding(this TransformsCatalog.CategoricalTransforms catalog,
+        ///     <summary>
+                ///     Convert several text column into hash-based one-hot encoded vectors.
+                ///     </summary>
+                ///     <param name="catalog">The transform catalog</param>
+                ///     <param name="columns">The column settings.</param>
+                ///     <returns></returns>
+                        public static OneHotHashEncodingEstimator OneHotHashEncoding(this TransformsCatalog.CategoricalTransforms catalog,
                 params OneHotHashEncodingEstimator.ColumnInfo[] columns)
             => new OneHotHashEncodingEstimator(CatalogUtils.GetEnvironment(catalog), columns);
     }
