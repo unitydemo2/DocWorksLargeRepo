cc8acc0d9447fadbcf427373513fd6274a888595
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 13:44:57 +0530

    DW CMS Message

--- src/Microsoft.ML.Data/EntryPoints/CommonOutputs.cs
+++ src/Microsoft.ML.Data/EntryPoints/CommonOutputs.cs
@@ -102,10 +102,10 @@ namespace Microsoft.ML.EntryPoints
         {
         }
 
-        /// <summary>
-        /// Marker interface for regression trainer output.
-        /// </summary>
-        public interface IRegressionOutput
+        ///     <summary>
+                ///     Marker interface for regression trainer output.
+                ///     </summary>
+                        public interface IRegressionOutput
         {
         }
 
