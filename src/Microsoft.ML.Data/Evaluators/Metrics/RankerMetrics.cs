53883375818bca213e570949676920b9394e6bc2
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 12:18:42 +0530

    DW CMS Message

--- src/Microsoft.ML.Data/Evaluators/Metrics/RankerMetrics.cs
+++ src/Microsoft.ML.Data/Evaluators/Metrics/RankerMetrics.cs
@@ -4,22 +4,14 @@
 
 namespace Microsoft.ML.Data
 {
+    
     public sealed class RankerMetrics
     {
-        /// <summary>
-        /// Array of normalized discounted cumulative gains where i-th element represent NDCG@i.
-        /// <image src="https://github.com/dotnet/machinelearning/tree/master/docs/images/NDCG.png"></image>
-        /// </summary>
-        public double[] Ndcg { get; }
+        /// <!-- Badly formed XML comment ignored for member "P:Microsoft.ML.Data.RankerMetrics.Ndcg" -->
+                        public double[] Ndcg { get; }
 
-        /// <summary>
-        ///Array of discounted cumulative gains where i-th element represent DCG@i.
-        /// <a href="https://en.wikipedia.org/wiki/Discounted_cumulative_gain">Discounted Cumulative gain</a>
-        /// is the sum of the gains, for all the instances i, normalized by the natural logarithm of the instance + 1.
-        /// Note that unline the Wikipedia article, ML.Net uses the natural logarithm.
-        /// <image src="https://github.com/dotnet/machinelearning/tree/master/docs/images/DCG.png"></image>
-        /// </summary>
-        public double[] Dcg { get; }
+        /// <!-- Badly formed XML comment ignored for member "P:Microsoft.ML.Data.RankerMetrics.Dcg" -->
+                        public double[] Dcg { get; }
 
         private static T Fetch<T>(IExceptionContext ectx, Row row, string name)
         {
