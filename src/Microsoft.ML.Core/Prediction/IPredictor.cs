90058d76f1e53b949c3e8f13983d85c367950b14
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 23:45:37 +0530

    DW CMS Message

--- src/Microsoft.ML.Core/Prediction/IPredictor.cs
+++ src/Microsoft.ML.Core/Prediction/IPredictor.cs
@@ -4,22 +4,33 @@
 
 namespace Microsoft.ML
 {
-    /// <summary>
-    /// Type of prediction task
-    /// </summary>
-    public enum PredictionKind
+    ///     <summary>
+        ///     Type of prediction task
+        ///     </summary>
+            public enum PredictionKind
     {
+        
         Unknown = 0,
+        
         Custom = 1,
 
+        
         BinaryClassification = 2,
+        
         MultiClassClassification = 3,
+        
         Regression = 4,
+        
         MultiOutputRegression = 5,
+        
         Ranking = 6,
+        
         Recommendation = 7,
+        
         AnomalyDetection = 8,
+        
         Clustering = 9,
+        
         SequenceClassification = 10,
 
         // More to be added later.
