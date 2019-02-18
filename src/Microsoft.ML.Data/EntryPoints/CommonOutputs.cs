3ca0f6763e4887abd17b73de19affab7dc363277
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 13:44:38 +0530

    DW CMS Message

--- src/Microsoft.ML.Data/EntryPoints/CommonOutputs.cs
+++ src/Microsoft.ML.Data/EntryPoints/CommonOutputs.cs
@@ -162,10 +162,10 @@ namespace Microsoft.ML.EntryPoints
         {
         }
 
-        /// <summary>
-        /// Marker interface for ranking trainer output.
-        /// </summary>
-        public interface IRankingOutput
+        ///     <summary>
+                ///     Marker interface for ranking trainer output.
+                ///     </summary>
+                        public interface IRankingOutput
         {
         }
 
