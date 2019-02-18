d7b4dfffbe1de89afc13e0d67d4ab2707464ea3d
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 11:49:30 +0530

    DW CMS Message

--- src/Microsoft.ML.Data/DataLoadSave/Text/TextLoaderStatic.cs
+++ src/Microsoft.ML.Data/DataLoadSave/Text/TextLoaderStatic.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -311,13 +311,16 @@ namespace Microsoft.ML.Data
         }
     }
 
+    
     public static class LocalPathReader
     {
+        
         public static IDataView Read(this IDataReader<IMultiStreamSource> reader, params string[] path)
         {
             return reader.Read(new MultiFileSource(path));
         }
 
+        
         public static DataView<TShape> Read<TShape>(this DataReader<IMultiStreamSource, TShape> reader, params string[] path)
         {
             return reader.Read(new MultiFileSource(path));
