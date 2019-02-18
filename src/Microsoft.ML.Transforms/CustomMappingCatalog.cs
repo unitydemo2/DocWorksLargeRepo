0a8ed4cec4e508d2b6ee296bafb447488d761b3b
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 22:22:44 +0530

    DW CMS Message

--- src/Microsoft.ML.Transforms/CustomMappingCatalog.cs
+++ src/Microsoft.ML.Transforms/CustomMappingCatalog.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -8,40 +8,20 @@ using Microsoft.ML.Transforms;
 
 namespace Microsoft.ML
 {
-    /// <summary>
-    /// Extension methods for custom mapping transformers.
-    /// </summary>
-    public static class CustomMappingCatalog
+    ///     <summary>
+        ///     Extension methods for custom mapping transformers.
+        ///     </summary>
+            public static class CustomMappingCatalog
     {
-        /// <summary>
-        /// Create a custom mapping of input columns to output columns.
-        /// </summary>
-        /// <typeparam name="TSrc">The class defining which columns to take from the incoming data.</typeparam>
-        /// <typeparam name="TDst">The class defining which new columns are added to the data.</typeparam>
-        /// <param name="catalog">The transform catalog</param>
-        /// <param name="mapAction">The mapping action. This must be thread-safe and free from side effects.</param>
-        /// <param name="contractName">The contract name, used by ML.NET for loading the model. If <c>null</c> is specified, such a trained model would not be save-able.</param>
-        /// <param name="inputSchemaDefinition">Additional parameters for schema mapping between <typeparamref name="TSrc"/> and input data.</param>
-        /// <param name="outputSchemaDefinition">Additional parameters for schema mapping between <typeparamref name="TDst"/> and output data.</param>
-        public static CustomMappingEstimator<TSrc, TDst> CustomMapping<TSrc, TDst>(this TransformsCatalog catalog, Action<TSrc, TDst> mapAction, string contractName,
+        /// <!-- Badly formed XML comment ignored for member "M:Microsoft.ML.CustomMappingCatalog.CustomMapping``2(Microsoft.ML.TransformsCatalog,System.Action{``0,``1},System.String,Microsoft.ML.Data.SchemaDefinition,Microsoft.ML.Data.SchemaDefinition)" -->
+                        public static CustomMappingEstimator<TSrc, TDst> CustomMapping<TSrc, TDst>(this TransformsCatalog catalog, Action<TSrc, TDst> mapAction, string contractName,
                 SchemaDefinition inputSchemaDefinition = null, SchemaDefinition outputSchemaDefinition = null)
             where TSrc : class, new()
             where TDst : class, new()
             => new CustomMappingEstimator<TSrc, TDst>(catalog.GetEnvironment(), mapAction, contractName, inputSchemaDefinition, outputSchemaDefinition);
 
-        /// <summary>
-        /// Create a custom mapping of input columns to output columns. Most likely, you should call this method when you are loading the model:
-        /// use <see cref="CustomMapping{TSrc, TDst}(TransformsCatalog, Action{TSrc, TDst}, string, SchemaDefinition, SchemaDefinition)"/> when you are
-        /// training the model.
-        /// </summary>
-        /// <typeparam name="TSrc">The class defining which columns to take from the incoming data.</typeparam>
-        /// <typeparam name="TDst">The class defining which new columns are added to the data.</typeparam>
-        /// <param name="catalog">The transform catalog</param>
-        /// <param name="mapAction">The mapping action. This must be thread-safe and free from side effects.</param>
-        /// <param name="contractName">The contract name, used by ML.NET for loading the model. If <c>null</c> is specified, such a trained model would not be save-able.</param>
-        /// <param name="inputSchemaDefinition">Additional parameters for schema mapping between <typeparamref name="TSrc"/> and input data.</param>
-        /// <param name="outputSchemaDefinition">Additional parameters for schema mapping between <typeparamref name="TDst"/> and output data.</param>
-        public static CustomMappingTransformer<TSrc, TDst> CustomMappingTransformer<TSrc, TDst>(this TransformsCatalog catalog, Action<TSrc, TDst> mapAction, string contractName,
+        /// <!-- Badly formed XML comment ignored for member "M:Microsoft.ML.CustomMappingCatalog.CustomMappingTransformer``2(Microsoft.ML.TransformsCatalog,System.Action{``0,``1},System.String,Microsoft.ML.Data.SchemaDefinition,Microsoft.ML.Data.SchemaDefinition)" -->
+                        public static CustomMappingTransformer<TSrc, TDst> CustomMappingTransformer<TSrc, TDst>(this TransformsCatalog catalog, Action<TSrc, TDst> mapAction, string contractName,
                 SchemaDefinition inputSchemaDefinition = null, SchemaDefinition outputSchemaDefinition = null)
             where TSrc : class, new()
             where TDst : class, new()
