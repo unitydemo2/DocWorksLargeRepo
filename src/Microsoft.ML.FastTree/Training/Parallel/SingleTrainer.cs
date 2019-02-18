55e94ef23736f2c091ea151a56f952f6c7781d00
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 19:28:41 +0530

    DW CMS Message

--- src/Microsoft.ML.FastTree/Training/Parallel/SingleTrainer.cs
+++ src/Microsoft.ML.FastTree/Training/Parallel/SingleTrainer.cs
@@ -19,18 +19,22 @@ namespace Microsoft.ML.Trainers.FastTree
     using LeafSplitCandidates = Internal.LeastSquaresRegressionTreeLearner.LeafSplitCandidates;
     using SplitInfo = Internal.LeastSquaresRegressionTreeLearner.SplitInfo;
 
+    
     public sealed class SingleTrainer : IParallelTraining
     {
+        
         public void CacheHistogram(bool isSmallerLeaf, int featureIdx, int subfeature, SufficientStatsBase sufficientStatsBase, bool hasWeights)
         {
             return;
         }
 
+        
         public bool IsNeedFindLocalBestSplit()
         {
             return true;
         }
 
+        
         public void FindGlobalBestSplit(LeafSplitCandidates smallerChildSplitCandidates,
             LeafSplitCandidates largerChildSplitCandidates,
             FindBestThresholdFromRawArrayFun findFunction,
@@ -39,16 +43,19 @@ namespace Microsoft.ML.Trainers.FastTree
             return;
         }
 
+        
         public void GetGlobalDataCountInLeaf(int leafIdx, ref int cnt)
         {
             return;
         }
 
+        
         public bool[] GetLocalBinConstructionFeatures(int numFeatures)
         {
             return Utils.CreateArray<bool>(numFeatures, true);
         }
 
+        
         public double[] GlobalMean(Dataset dataset, RegressionTree tree, DocumentPartitioning partitioning, double[] weights, bool filterZeroLambdas)
         {
             double[] means = new double[tree.NumLeaves];
@@ -59,46 +66,55 @@ namespace Microsoft.ML.Trainers.FastTree
             return means;
         }
 
+        
         public void PerformGlobalSplit(int leaf, int lteChild, int gtChild, SplitInfo splitInfo)
         {
             return;
         }
 
+        
         public void InitIteration(ref bool[] activeFeatures)
         {
             return;
         }
 
+        
         public void InitEnvironment()
         {
             return;
         }
 
+        
         public void InitTreeLearner(Dataset trainData, int maxNumLeaves, int maxCatSplitPoints, ref int minDocInLeaf)
         {
             return;
         }
 
+        
         public void SyncGlobalBoundary(int numFeatures, int maxBin, Double[][] binUpperBounds)
         {
             return;
         }
 
+        
         public void FinalizeEnvironment()
         {
             return;
         }
 
+        
         public void FinalizeTreeLearner()
         {
             return;
         }
 
+        
         public void FinalizeIteration()
         {
             return;
         }
 
+        
         public bool IsSkipNonSplittableHistogram()
         {
             return true;
