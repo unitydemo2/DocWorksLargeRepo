6b7b016c0f8e42d04a98f5e094dff0cf802152e3
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 14:51:35 +0530

    DW CMS Message

--- src/Microsoft.ML.StandardLearners/Standard/StochasticTrainerBase.cs
+++ src/Microsoft.ML.StandardLearners/Standard/StochasticTrainerBase.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -11,23 +11,27 @@ using Microsoft.ML.Transforms;
 
 namespace Microsoft.ML.Learners
 {
+    
     public abstract class StochasticTrainerBase<TTransformer, TModel> : TrainerEstimatorBase<TTransformer, TModel>
         where TTransformer : ISingleFeaturePredictionTransformer<TModel>
         where TModel : IPredictor
     {
+        
         public StochasticTrainerBase(IHost host, SchemaShape.Column feature, SchemaShape.Column label, SchemaShape.Column weight = default)
             : base(host, feature, label, weight)
         {
         }
 
-        /// <summary>
-        /// Whether data is to be shuffled every epoch.
-        /// </summary>
-        protected abstract bool ShuffleData { get; }
+        ///     <summary>
+                ///     Whether data is to be shuffled every epoch.
+                ///     </summary>
+                        protected abstract bool ShuffleData { get; }
 
         private static readonly TrainerInfo _info = new TrainerInfo();
+        
         public override TrainerInfo Info => _info;
 
+        
         private protected override TModel TrainModelCore(TrainContext context)
         {
             Host.CheckValue(context, nameof(context));
@@ -43,6 +47,7 @@ namespace Microsoft.ML.Learners
             }
         }
 
+        
         private protected virtual int ComputeNumThreads(FloatLabelCursor.Factory cursorFactory)
         {
             int maxThreads = Math.Min(8, Math.Max(1, Environment.ProcessorCount / 2));
@@ -52,16 +57,16 @@ namespace Microsoft.ML.Learners
             return maxThreads;
         }
 
-        /// <summary>
-        /// This method ensures that the data meets the requirements of this trainer and its
-        /// subclasses, injects necessary transforms, and throws if it couldn't meet them.
-        /// </summary>
-        /// <param name="ch">The channel</param>
-        /// <param name="examples">The training examples</param>
-        /// <param name="weightSetCount">Gets the length of weights and bias array. For binary classification and regression,
-        /// this is 1. For multi-class classification, this equals the number of classes on the label.</param>
-        /// <returns>A potentially modified version of <paramref name="examples"/></returns>
-        private protected RoleMappedData PrepareDataFromTrainingExamples(IChannel ch, RoleMappedData examples, out int weightSetCount)
+        ///     <summary>
+                ///     This method ensures that the data meets the requirements of this trainer and its
+                ///     subclasses, injects necessary transforms, and throws if it couldn't meet them.
+                ///     </summary>
+                ///     <param name="ch">The channel</param>
+                ///     <param name="examples">The training examples</param>
+                ///     <param name="weightSetCount">Gets the length of weights and bias array. For binary classification and regression,
+                ///     this is 1. For multi-class classification, this equals the number of classes on the label.</param>
+                ///     <returns>A potentially modified version of <paramref name="examples"/></returns>
+                        private protected RoleMappedData PrepareDataFromTrainingExamples(IChannel ch, RoleMappedData examples, out int weightSetCount)
         {
             ch.AssertValue(examples);
             CheckLabel(examples, out weightSetCount);
@@ -94,8 +99,10 @@ namespace Microsoft.ML.Learners
             return examplesToFeedTrain;
         }
 
+        
         private protected abstract TModel TrainCore(IChannel ch, RoleMappedData data, LinearModelParameters predictor, int weightSetCount);
 
+        
         private protected abstract void CheckLabel(RoleMappedData examples, out int weightSetCount);
     }
 }
