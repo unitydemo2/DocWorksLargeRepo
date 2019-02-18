85e941dc260d67c7c9bf71a83309f911ee66ca35
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 18:58:24 +0530

    DW CMS Message

--- src/Microsoft.ML.FastTree/Utils/PseudorandomFunction.cs
+++ src/Microsoft.ML.FastTree/Utils/PseudorandomFunction.cs
@@ -7,21 +7,23 @@ using System.Linq;
 
 namespace Microsoft.ML.Trainers.FastTree.Internal
 {
-    /// <summary>
-    /// This class defines a psuedorandom function, mapping a number to
-    /// a hard to predict but deterministic other number, through some
-    /// nefarious means.
-    /// </summary>
-    public sealed class PseudorandomFunction
+    ///     <summary>
+        ///     This class defines a psuedorandom function, mapping a number to
+        ///     a hard to predict but deterministic other number, through some
+        ///     nefarious means.
+        ///     </summary>
+            public sealed class PseudorandomFunction
     {
         private readonly int[][] _data;
         private static readonly int[] _periodics = new int[] { 32, 27, 25, 49, 11, 13, 17, 23, 29, 31, 37, 41, 43, 47 };
 
+        
         public PseudorandomFunction(Random rand)
         {
             _data = _periodics.Select(x => Enumerable.Range(0, x).Select(y => rand.Next(-1, int.MaxValue) + 1).ToArray()).ToArray();
         }
 
+        
         public int Apply(ulong seed)
         {
             int val = 0;
