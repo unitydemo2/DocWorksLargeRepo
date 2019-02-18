dcaa8ab7fdc00fb0a7874b408e7da66d8ee26b88
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 12:23:32 +0530

    DW CMS Message

--- src/Microsoft.ML.Data/Evaluators/Metrics/RegressionMetrics.cs
+++ src/Microsoft.ML.Data/Evaluators/Metrics/RegressionMetrics.cs
@@ -4,51 +4,52 @@
 
 namespace Microsoft.ML.Data
 {
+    
     public sealed class RegressionMetrics
     {
-        /// <summary>
-        /// Gets the absolute loss of the model.
-        /// </summary>
-        /// <remarks>
-        /// The absolute loss is defined as
-        /// L1 = (1/m) * sum( abs( yi - y&apos;i))
-        /// where m is the number of instances in the test set.
-        /// y'i are the predicted labels for each instance.
-        /// yi are the correct labels of each instance.
-        /// </remarks>
-        public double L1 { get; }
+        ///     <summary>
+                ///     Gets the absolute loss of the model.
+                ///     </summary>
+                ///     <remarks>
+                ///     The absolute loss is defined as
+                ///     L1 = (1/m) * sum( abs( yi - y'i))
+                ///     where m is the number of instances in the test set.
+                ///     y'i are the predicted labels for each instance.
+                ///     yi are the correct labels of each instance.
+                ///     </remarks>
+                        public double L1 { get; }
 
-        /// <summary>
-        /// Gets the squared loss of the model.
-        /// </summary>
-        /// <remarks>
-        /// The squared loss is defined as
-        /// L2 = (1/m) * sum(( yi - y&apos;i)^2)
-        /// where m is the number of instances in the test set.
-        /// y'i are the predicted labels for each instance.
-        /// yi are the correct labels of each instance.
-        /// </remarks>
-        public double L2 { get; }
+        ///     <summary>
+                ///     Gets the squared loss of the model.
+                ///     </summary>
+                ///     <remarks>
+                ///     The squared loss is defined as
+                ///     L2 = (1/m) * sum(( yi - y'i)^2)
+                ///     where m is the number of instances in the test set.
+                ///     y'i are the predicted labels for each instance.
+                ///     yi are the correct labels of each instance.
+                ///     </remarks>
+                        public double L2 { get; }
 
-        /// <summary>
-        /// Gets the root mean square loss (or RMS) which is the square root of the L2 loss.
-        /// </summary>
-        public double Rms { get; }
+        ///     <summary>
+                ///     Gets the root mean square loss (or RMS) which is the square root of the L2 loss.
+                ///     </summary>
+                        public double Rms { get; }
 
-        /// <summary>
-        /// Gets the result of user defined loss function.
-        /// </summary>
-        /// <remarks>
-        /// This is the average of a loss function defined by the user,
-        /// computed over all the instances in the test set.
-        /// </remarks>
-        public double LossFn { get; }
+        ///     <summary>
+                ///     Gets the result of user defined loss function.
+                ///     </summary>
+                ///     <remarks>
+                ///     This is the average of a loss function defined by the user,
+                ///     computed over all the instances in the test set.
+                ///     </remarks>
+                        public double LossFn { get; }
 
-        /// <summary>
-        /// Gets the R squared value of the model, which is also known as
-        /// the coefficient of determination​.
-        /// </summary>
-        public double RSquared { get; }
+        ///     <summary>
+                ///     Gets the R squared value of the model, which is also known as
+                ///     the coefficient of determination​.
+                ///     </summary>
+                        public double RSquared { get; }
 
         internal RegressionMetrics(IExceptionContext ectx, Row overallResult)
         {
