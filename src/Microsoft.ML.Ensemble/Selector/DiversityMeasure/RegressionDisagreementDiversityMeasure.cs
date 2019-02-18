7ea47190cdf08e5f178507a9dd791f0329989a45
CMSQATeam <cmsqa123@gmail.com>
2019-02-18 13:21:24 +0530

    DW CMS Message

--- src/Microsoft.ML.Ensemble/Selector/DiversityMeasure/RegressionDisagreementDiversityMeasure.cs
+++ src/Microsoft.ML.Ensemble/Selector/DiversityMeasure/RegressionDisagreementDiversityMeasure.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -12,10 +12,13 @@ using Microsoft.ML.Ensemble.Selector.DiversityMeasure;
 
 namespace Microsoft.ML.Ensemble.Selector.DiversityMeasure
 {
+    
     public class RegressionDisagreementDiversityMeasure : BaseDisagreementDiversityMeasure<Single>, IRegressionDiversityMeasure
     {
+        
         public const string LoadName = "RegressionDisagreementDiversityMeasure";
 
+        
         protected override Single GetDifference(in Single valueX, in Single valueY)
         {
             return Math.Abs(valueX - valueY);
