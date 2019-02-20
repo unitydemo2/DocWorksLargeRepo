256bf536d89db7b357267cc39a6ecffe644991e8
CMSQATeam <cmsqa123@gmail.com>
2019-02-20 13:21:43 +0530

    DW CMS Message

--- src/Microsoft.ML.Ensemble/EntryPoints/DiversityMeasure.cs
+++ src/Microsoft.ML.Ensemble/EntryPoints/DiversityMeasure.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -13,9 +13,11 @@ using Microsoft.ML.EntryPoints;
 
 namespace Microsoft.ML.Ensemble.EntryPoints
 {
+    
     [TlcModule.Component(Name = DisagreementDiversityMeasure.LoadName, FriendlyName = DisagreementDiversityMeasure.UserName)]
     public sealed class DisagreementDiversityFactory : ISupportBinaryDiversityMeasureFactory
     {
+        
         public IBinaryDiversityMeasure CreateComponent(IHostEnvironment env) => new DisagreementDiversityMeasure();
     }
 
