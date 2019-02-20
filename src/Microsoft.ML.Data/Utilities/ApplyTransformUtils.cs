4bd58229dd63cf45f651d2d8f89f19d5e5e71787
CMSQATeam <cmsqa123@gmail.com>
2019-02-20 11:23:39 +0530

    DW CMS Message

--- src/Microsoft.ML.Data/Utilities/ApplyTransformUtils.cs
+++ src/Microsoft.ML.Data/Utilities/ApplyTransformUtils.cs
@@ -8,21 +8,21 @@ using Microsoft.ML.Model;
 
 namespace Microsoft.ML.Data
 {
-    /// <summary>
-    /// Utilities to rebind data transforms
-    /// </summary>
-    public static class ApplyTransformUtils
+    ///     <summary>
+        ///     Utilities to rebind data transforms
+        ///     </summary>
+            public static class ApplyTransformUtils
     {
-        /// <summary>
-        /// Attempt to apply the data transform to a different data view source.
-        /// If the transform in question implements <see cref="ITransformTemplate"/>, <see cref="ITransformTemplate.ApplyToData"/>
-        /// is called. Otherwise, the transform is serialized into a byte array and then deserialized.
-        /// </summary>
-        /// <param name="env">The host to use</param>
-        /// <param name="transform">The transform to apply.</param>
-        /// <param name="newSource">The data view to apply the transform to.</param>
-        /// <returns>The resulting data view.</returns>
-        public static IDataTransform ApplyTransformToData(IHostEnvironment env, IDataTransform transform, IDataView newSource)
+        ///     <summary>
+                ///     Attempt to apply the data transform to a different data view source.
+                ///     If the transform in question implements <see cref="ITransformTemplate"/>, <see cref="ITransformTemplate.ApplyToData"/>
+                ///     is called. Otherwise, the transform is serialized into a byte array and then deserialized.
+                ///     </summary>
+                ///     <param name="env">The host to use</param>
+                ///     <param name="transform">The transform to apply.</param>
+                ///     <param name="newSource">The data view to apply the transform to.</param>
+                ///     <returns>The resulting data view.</returns>
+                        public static IDataTransform ApplyTransformToData(IHostEnvironment env, IDataTransform transform, IDataView newSource)
         {
             Contracts.CheckValue(env, nameof(env));
             env.CheckValue(transform, nameof(transform));
@@ -51,19 +51,8 @@ namespace Microsoft.ML.Data
             }
         }
 
-        /// <summary>
-        /// Walks back the Source chain of the <see cref="IDataTransform"/> up to the <paramref name="oldSource"/>
-        /// (or <see cref="IDataLoader"/> if <paramref name="oldSource"/> is <c>null</c>),
-        /// and reapplies all transforms in the chain, to produce the same chain but bound to the different data.
-        /// It is valid to have no transforms: in this case the result will be equal to <paramref name="newSource"/>
-        /// If <paramref name="oldSource"/> is specified and not found in the pipe, an exception is thrown.
-        /// </summary>
-        /// <param name="env">The environment to use.</param>
-        /// <param name="chain">The end of the chain.</param>
-        /// <param name="newSource">The new data to attach the chain to.</param>
-        /// <param name="oldSource">The 'old source' of the pipe, that doesn't need to be reapplied. If null, all transforms are reapplied.</param>
-        /// <returns>The resulting data view.</returns>
-        public static IDataView ApplyAllTransformsToData(IHostEnvironment env, IDataView chain, IDataView newSource, IDataView oldSource = null)
+        /// <!-- Badly formed XML comment ignored for member "M:Microsoft.ML.Data.ApplyTransformUtils.ApplyAllTransformsToData(Microsoft.ML.IHostEnvironment,Microsoft.ML.Data.IDataView,Microsoft.ML.Data.IDataView,Microsoft.ML.Data.IDataView)" -->
+                        public static IDataView ApplyAllTransformsToData(IHostEnvironment env, IDataView chain, IDataView newSource, IDataView oldSource = null)
         {
             // REVIEW: have a variation that would selectively apply transforms?
             Contracts.CheckValue(env, nameof(env));
