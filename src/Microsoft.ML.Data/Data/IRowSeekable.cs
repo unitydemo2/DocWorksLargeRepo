0ae94b83c304d161b6cefe60eb7a31acfbd40278
CMSQATeam <cmsqa123@gmail.com>
2019-02-20 12:43:44 +0530

    DW CMS Message

--- src/Microsoft.ML.Data/Data/IRowSeekable.cs
+++ src/Microsoft.ML.Data/Data/IRowSeekable.cs
@@ -18,20 +18,20 @@ namespace Microsoft.ML.Data
         Schema Schema { get; }
     }
 
-    /// <summary>
-    /// Represents a row seeker with random access that can retrieve a specific row by the row index.
-    /// For <see cref="RowSeeker"/>, when the state is valid (that is when <see cref="MoveTo(long)"/>
-    /// returns <see langword="true"/>), it returns the current row index. Otherwise it's -1.
-    /// </summary>
-    public abstract class RowSeeker : Row
+    ///     <summary>
+        ///     Represents a row seeker with random access that can retrieve a specific row by the row index.
+        ///     For <see cref="RowSeeker"/>, when the state is valid (that is when <see cref="MoveTo(long)"/>
+        ///     returns <see langword="true"/>), it returns the current row index. Otherwise it's -1.
+        ///     </summary>
+            public abstract class RowSeeker : Row
     {
-        /// <summary>
-        /// Moves the seeker to a row at a specific row index.
-        /// If the row index specified is out of range (less than zero or not less than the
-        /// row count), it returns false and sets its Position property to -1.
-        /// </summary>
-        /// <param name="rowIndex">The row index to move to.</param>
-        /// <returns>True if a row with specified index is found; false otherwise.</returns>
-        public abstract bool MoveTo(long rowIndex);
+        ///     <summary>
+                ///     Moves the seeker to a row at a specific row index.
+                ///     If the row index specified is out of range (less than zero or not less than the
+                ///     row count), it returns false and sets its Position property to -1.
+                ///     </summary>
+                ///     <param name="rowIndex">The row index to move to.</param>
+                ///     <returns>True if a row with specified index is found; false otherwise.</returns>
+                        public abstract bool MoveTo(long rowIndex);
     }
 }
