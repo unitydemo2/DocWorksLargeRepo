6455b63e4629b6702ef36a57221d1e1c7af8468c
CMSQATeam <cmsqa123@gmail.com>
2019-02-20 13:39:28 +0530

    DW CMS Message

--- src/Microsoft.ML.Ensemble/Selector/DiversityMeasure/ModelDiversityMetric.cs
+++ src/Microsoft.ML.Ensemble/Selector/DiversityMeasure/ModelDiversityMetric.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -6,10 +6,14 @@ using System;
 
 namespace Microsoft.ML.Ensemble.Selector.DiversityMeasure
 {
+    
     public class ModelDiversityMetric<TOutput>
     {
+        
         public FeatureSubsetModel<IPredictorProducing<TOutput>> ModelX { get; set; }
+        
         public FeatureSubsetModel<IPredictorProducing<TOutput>> ModelY { get; set; }
+        
         public Single DiversityNumber { get; set; }
     }
 }
