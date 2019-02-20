22ef874cd2d4eba694b131f3e45cfdf53050387e
CMSQATeam <cmsqa123@gmail.com>
2019-02-20 17:30:41 +0530

    DW CMS Message

--- src/Microsoft.ML.Legacy/Trainers/LightGBM.cs
+++ src/Microsoft.ML.Legacy/Trainers/LightGBM.cs
@@ -1,18 +1,10 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
 namespace Microsoft.ML.Legacy.Trainers
 {
-    /// <summary>
-    /// This API requires Microsoft.ML.LightGBM nuget.
-    /// </summary>
-    /// <example>
-    /// <code>
-    /// pipeline.Add(new LightGbmBinaryClassifier() { NumLeaves = 5, NumBoostRound = 5, MinDataPerLeaf = 2 })
-    /// </code>
-    /// </example>
-    public sealed partial class LightGbmBinaryClassifier
+        public sealed partial class LightGbmBinaryClassifier
     {
 
     }
