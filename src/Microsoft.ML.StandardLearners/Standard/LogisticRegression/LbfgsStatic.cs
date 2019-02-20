f78d3299873af57d736cfdfe187e49500dc1cd3a
CMSQATeam <cmsqa123@gmail.com>
2019-02-20 19:35:58 +0530

    DW CMS Message

--- src/Microsoft.ML.StandardLearners/Standard/LogisticRegression/LbfgsStatic.cs
+++ src/Microsoft.ML.StandardLearners/Standard/LogisticRegression/LbfgsStatic.cs
@@ -121,32 +121,32 @@ namespace Microsoft.ML.StaticPipe
         }
     }
 
-    /// <summary>
-    /// Multiclass Classification trainer estimators.
-    /// </summary>
-    public static class LbfgsMulticlassExtensions
+    ///     <summary>
+        ///     Multiclass Classification trainer estimators.
+        ///     </summary>
+            public static class LbfgsMulticlassExtensions
     {
 
-        /// <summary>
-        /// Predict a target using a linear multiclass classification model trained with the <see cref="Microsoft.ML.Learners.MulticlassLogisticRegression"/> trainer.
-        /// </summary>
-        /// <param name="ctx">The multiclass classification context trainer object.</param>
-        /// <param name="label">The label, or dependent variable.</param>
-        /// <param name="features">The features, or independent variables.</param>
-        /// <param name="weights">The optional example weights.</param>
-        /// <param name="enoforceNoNegativity">Enforce non-negative weights.</param>
-        /// <param name="l1Weight">Weight of L1 regularization term.</param>
-        /// <param name="l2Weight">Weight of L2 regularization term.</param>
-        /// <param name="memorySize">Memory size for <see cref="Microsoft.ML.Learners.LogisticRegression"/>. Low=faster, less accurate.</param>
-        /// <param name="optimizationTolerance">Threshold for optimizer convergence.</param>
-        /// <param name="advancedSettings">A delegate to apply all the advanced arguments to the algorithm.</param>
-        /// <param name="onFit">A delegate that is called every time the
-        /// <see cref="Estimator{TInShape, TOutShape, TTransformer}.Fit(DataView{TInShape})"/> method is called on the
-        /// <see cref="Estimator{TInShape, TOutShape, TTransformer}"/> instance created out of this. This delegate will receive
-        /// the linear model that was trained. Note that this action cannot change the
-        /// result in any way; it is only a way for the caller to be informed about what was learnt.</param>
-        /// <returns>The set of output columns including in order the predicted per-class likelihoods (between 0 and 1, and summing up to 1), and the predicted label.</returns>
-        public static (Vector<float> score, Key<uint, TVal> predictedLabel)
+        ///     <summary>
+                ///     Predict a target using a linear multiclass classification model trained with the <see cref="Microsoft.ML.Learners.MulticlassLogisticRegression"/> trainer.
+                ///     </summary>
+                ///     <param name="ctx">The multiclass classification context trainer object.</param>
+                ///     <param name="label">The label, or dependent variable.</param>
+                ///     <param name="features">The features, or independent variables.</param>
+                ///     <param name="weights">The optional example weights.</param>
+                ///     <param name="enoforceNoNegativity">Enforce non-negative weights.</param>
+                ///     <param name="l1Weight">Weight of L1 regularization term.</param>
+                ///     <param name="l2Weight">Weight of L2 regularization term.</param>
+                ///     <param name="memorySize">Memory size for <see cref="Microsoft.ML.Learners.LogisticRegression"/>. Low=faster, less accurate.</param>
+                ///     <param name="optimizationTolerance">Threshold for optimizer convergence.</param>
+                ///     <param name="advancedSettings">A delegate to apply all the advanced arguments to the algorithm.</param>
+                ///     <param name="onFit">A delegate that is called every time the
+                ///     <see cref="Estimator{TInShape, TOutShape, TTransformer}.Fit(DataView{TInShape})"/> method is called on the
+                ///     <see cref="Estimator{TInShape, TOutShape, TTransformer}"/> instance created out of this. This delegate will receive
+                ///     the linear model that was trained. Note that this action cannot change the
+                ///     result in any way; it is only a way for the caller to be informed about what was learnt.</param>
+                ///     <returns>The set of output columns including in order the predicted per-class likelihoods (between 0 and 1, and summing up to 1), and the predicted label.</returns>
+                        public static (Vector<float> score, Key<uint, TVal> predictedLabel)
             MultiClassLogisticRegression<TVal>(this MulticlassClassificationContext.MulticlassClassificationTrainers ctx,
             Key<uint, TVal> label,
             Vector<float> features,
