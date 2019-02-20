49d138281e4860ee6b5a3c02db63c31c562bb0ec
CMSQATeam <cmsqa123@gmail.com>
2019-02-20 11:00:43 +0530

    DW CMS Message

--- src/Microsoft.ML.Data/Prediction/CalibratorCatalog.cs
+++ src/Microsoft.ML.Data/Prediction/CalibratorCatalog.cs
@@ -441,10 +441,10 @@ namespace Microsoft.ML.Calibrator
 
     }
 
-    /// <summary>
-    /// The <see cref="ITransformer"/> implementation obtained by training a <see cref="PavCalibratorEstimator"/>
-    /// </summary>
-    public sealed class PavCalibratorTransformer : CalibratorTransformer<PavCalibrator>
+    ///     <summary>
+        ///     The <see cref="ITransformer"/> implementation obtained by training a <see cref="PavCalibratorEstimator"/>
+        ///     </summary>
+            public sealed class PavCalibratorTransformer : CalibratorTransformer<PavCalibrator>
     {
         internal const string LoadName = "PavCalibratTransf";
 
