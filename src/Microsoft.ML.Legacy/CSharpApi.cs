000904293eec0bc467f93319e597cac83184e747
CMSQATeam <cmsqa123@gmail.com>
2019-02-20 18:44:13 +0530

    DW CMS Message

--- src/Microsoft.ML.Legacy/CSharpApi.cs
+++ src/Microsoft.ML.Legacy/CSharpApi.cs
@@ -18041,11 +18041,15 @@ namespace Microsoft.ML
 
     namespace Legacy.Transforms
     {
+        
         [Obsolete]
         public enum NgramExtractingEstimatorWeightingCriteria
         {
+            
             Tf = 0,
+            
             Idf = 1,
+            
             TfIdf = 2
         }
 
