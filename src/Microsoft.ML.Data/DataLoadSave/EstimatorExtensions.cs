c78bd046fc914bbbde53f755045b3dd21b0fa67c
CMSQATeam <cmsqa123@gmail.com>
2019-02-21 00:09:59 +0530

    DW CMS Message

--- src/Microsoft.ML.Data/DataLoadSave/EstimatorExtensions.cs
+++ src/Microsoft.ML.Data/DataLoadSave/EstimatorExtensions.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -9,15 +9,15 @@ using Microsoft.ML.Internal.Utilities;
 
 namespace Microsoft.ML
 {
-    /// <summary>
-    /// Extension methods that allow chaining estimator and transformer pipes together.
-    /// </summary>
-    public static class LearningPipelineExtensions
+    ///     <summary>
+        ///     Extension methods that allow chaining estimator and transformer pipes together.
+        ///     </summary>
+            public static class LearningPipelineExtensions
     {
-        /// <summary>
-        /// Create a new composite reader estimator, by appending another estimator to the end of this data reader estimator.
-        /// </summary>
-        public static CompositeReaderEstimator<TSource, TTrans> Append<TSource, TTrans>(
+        ///     <summary>
+                ///     Create a new composite reader estimator, by appending another estimator to the end of this data reader estimator.
+                ///     </summary>
+                        public static CompositeReaderEstimator<TSource, TTrans> Append<TSource, TTrans>(
             this IDataReaderEstimator<TSource, IDataReader<TSource>> start, IEstimator<TTrans> estimator)
             where TTrans : class, ITransformer
         {
@@ -27,10 +27,10 @@ namespace Microsoft.ML
             return new CompositeReaderEstimator<TSource, ITransformer>(start).Append(estimator);
         }
 
-        /// <summary>
-        /// Create a new composite reader estimator, by appending an estimator to this data reader.
-        /// </summary>
-        public static CompositeReaderEstimator<TSource, TTrans> Append<TSource, TTrans>(
+        ///     <summary>
+                ///     Create a new composite reader estimator, by appending an estimator to this data reader.
+                ///     </summary>
+                        public static CompositeReaderEstimator<TSource, TTrans> Append<TSource, TTrans>(
             this IDataReader<TSource> start, IEstimator<TTrans> estimator)
             where TTrans : class, ITransformer
         {
@@ -40,10 +40,10 @@ namespace Microsoft.ML
             return new TrivialReaderEstimator<TSource, IDataReader<TSource>>(start).Append(estimator);
         }
 
-        /// <summary>
-        /// Create a new estimator chain, by appending another estimator to the end of this estimator.
-        /// </summary>
-        public static EstimatorChain<TTrans> Append<TTrans>(
+        ///     <summary>
+                ///     Create a new estimator chain, by appending another estimator to the end of this estimator.
+                ///     </summary>
+                        public static EstimatorChain<TTrans> Append<TTrans>(
             this IEstimator<ITransformer> start, IEstimator<TTrans> estimator,
             TransformerScope scope = TransformerScope.Everything)
             where TTrans : class, ITransformer
@@ -57,13 +57,13 @@ namespace Microsoft.ML
             return new EstimatorChain<ITransformer>().Append(start).Append(estimator, scope);
         }
 
-        /// <summary>
-        /// Append a 'caching checkpoint' to the estimator chain. This will ensure that the downstream estimators will be trained against
-        /// cached data. It is helpful to have a caching checkpoint before trainers that take multiple data passes.
-        /// </summary>
-        /// <param name="start">The starting estimator</param>
-        /// <param name="env">The host environment to use for caching.</param>
-
+        ///     <summary>
+        ///     Append a 'caching checkpoint' to the estimator chain. This will ensure that the downstream estimators will be trained against
+        ///     cached data. It is helpful to have a caching checkpoint before trainers that take multiple data passes.
+        ///     </summary>
+        ///     <param name="start">The starting estimator</param>
+        ///     <param name="env">The host environment to use for caching.</param>
+        
         public static EstimatorChain<TTrans> AppendCacheCheckpoint<TTrans>(this IEstimator<TTrans> start, IHostEnvironment env)
             where TTrans : class, ITransformer
         {
@@ -71,10 +71,10 @@ namespace Microsoft.ML
             return new EstimatorChain<ITransformer>().Append(start).AppendCacheCheckpoint(env);
         }
 
-        /// <summary>
-        /// Create a new composite reader, by appending a transformer to this data reader.
-        /// </summary>
-        public static CompositeDataReader<TSource, TTrans> Append<TSource, TTrans>(this IDataReader<TSource> reader, TTrans transformer)
+        ///     <summary>
+                ///     Create a new composite reader, by appending a transformer to this data reader.
+                ///     </summary>
+                        public static CompositeDataReader<TSource, TTrans> Append<TSource, TTrans>(this IDataReader<TSource> reader, TTrans transformer)
             where TTrans : class, ITransformer
         {
             Contracts.CheckValue(reader, nameof(reader));
@@ -83,10 +83,10 @@ namespace Microsoft.ML
             return new CompositeDataReader<TSource, ITransformer>(reader).AppendTransformer(transformer);
         }
 
-        /// <summary>
-        /// Create a new transformer chain, by appending another transformer to the end of this transformer chain.
-        /// </summary>
-        public static TransformerChain<TTrans> Append<TTrans>(this ITransformer start, TTrans transformer)
+        ///     <summary>
+                ///     Create a new transformer chain, by appending another transformer to the end of this transformer chain.
+                ///     </summary>
+                        public static TransformerChain<TTrans> Append<TTrans>(this ITransformer start, TTrans transformer)
             where TTrans : class, ITransformer
         {
             Contracts.CheckValue(start, nameof(start));
@@ -120,22 +120,22 @@ namespace Microsoft.ML
                 => _est.GetOutputSchema(inputSchema);
         }
 
-        /// <summary>
-        /// Given an estimator, return a wrapping object that will call a delegate once <see cref="IEstimator{TTransformer}.Fit(IDataView)"/>
-        /// is called. It is often important for an estimator to return information about what was fit, which is why the
-        /// <see cref="IEstimator{TTransformer}.Fit(IDataView)"/> method returns a specifically typed object, rather than just a general
-        /// <see cref="ITransformer"/>. However, at the same time, <see cref="IEstimator{TTransformer}"/> are often formed into pipelines
-        /// with many objects, so we may need to build a chain of estimators via <see cref="EstimatorChain{TLastTransformer}"/> where the
-        /// estimator for which we want to get the transformer is buried somewhere in this chain. For that scenario, we can through this
-        /// method attach a delegate that will be called once fit is called.
-        /// </summary>
-        /// <typeparam name="TTransformer">The type of <see cref="ITransformer"/> returned by <paramref name="estimator"/></typeparam>
-        /// <param name="estimator">The estimator to wrap</param>
-        /// <param name="onFit">The delegate that is called with the resulting <typeparamref name="TTransformer"/> instances once
-        /// <see cref="IEstimator{TTransformer}.Fit(IDataView)"/> is called. Because <see cref="IEstimator{TTransformer}.Fit(IDataView)"/>
-        /// may be called multiple times, this delegate may also be called multiple times.</param>
-        /// <returns>A wrapping estimator that calls the indicated delegate whenever fit is called</returns>
-        public static IEstimator<TTransformer> WithOnFitDelegate<TTransformer>(this IEstimator<TTransformer> estimator, Action<TTransformer> onFit)
+        ///     <summary>
+                ///     Given an estimator, return a wrapping object that will call a delegate once <see cref="IEstimator{TTransformer}.Fit(IDataView)"/>
+                ///     is called. It is often important for an estimator to return information about what was fit, which is why the
+                ///     <see cref="IEstimator{TTransformer}.Fit(IDataView)"/> method returns a specifically typed object, rather than just a general
+                ///     <see cref="ITransformer"/>. However, at the same time, <see cref="IEstimator{TTransformer}"/> are often formed into pipelines
+                ///     with many objects, so we may need to build a chain of estimators via <see cref="EstimatorChain{TLastTransformer}"/> where the
+                ///     estimator for which we want to get the transformer is buried somewhere in this chain. For that scenario, we can through this
+                ///     method attach a delegate that will be called once fit is called.
+                ///     </summary>
+                ///     <typeparam name="TTransformer">The type of <see cref="ITransformer"/> returned by <paramref name="estimator"/></typeparam>
+                ///     <param name="estimator">The estimator to wrap</param>
+                ///     <param name="onFit">The delegate that is called with the resulting <typeparamref name="TTransformer"/> instances once
+                ///     <see cref="IEstimator{TTransformer}.Fit(IDataView)"/> is called. Because <see cref="IEstimator{TTransformer}.Fit(IDataView)"/>
+                ///     may be called multiple times, this delegate may also be called multiple times.</param>
+                ///     <returns>A wrapping estimator that calls the indicated delegate whenever fit is called</returns>
+                        public static IEstimator<TTransformer> WithOnFitDelegate<TTransformer>(this IEstimator<TTransformer> estimator, Action<TTransformer> onFit)
             where TTransformer : class, ITransformer
         {
             Contracts.CheckValue(estimator, nameof(estimator));
