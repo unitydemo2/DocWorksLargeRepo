2422d0826ad37661d08d2bc074a9c99c7220b32d
CMSQATeam <cmsqa123@gmail.com>
2019-02-20 20:57:45 +0530

    DW CMS Message

--- src/Microsoft.ML.FastTree/Training/EnsembleCompression/IEnsembleCompressor.cs
+++ src/Microsoft.ML.FastTree/Training/EnsembleCompression/IEnsembleCompressor.cs
@@ -1,17 +1,22 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
 namespace Microsoft.ML.Trainers.FastTree.Internal
 {
+    
     public interface IEnsembleCompressor<TLabel>
     {
+        
         void Initialize(int numTrees, Dataset trainSet, TLabel[] labels, int randomSeed);
 
+        
         void SetTreeScores(int idx, double[] scores);
 
+        
         bool Compress(IChannel ch, TreeEnsemble ensemble, double[] trainScores, int bestIteration, int maxTreesAfterCompression);
 
+        
         TreeEnsemble GetCompressedEnsemble();
     }
 }
