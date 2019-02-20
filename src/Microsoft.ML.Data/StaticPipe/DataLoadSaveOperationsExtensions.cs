8c8976ca13306f0a8f30944e460db1e33e4b244e
CMSQATeam <cmsqa123@gmail.com>
2019-02-20 19:30:20 +0530

    DW CMS Message

--- src/Microsoft.ML.Data/StaticPipe/DataLoadSaveOperationsExtensions.cs
+++ src/Microsoft.ML.Data/StaticPipe/DataLoadSaveOperationsExtensions.cs
@@ -8,31 +8,11 @@ using static Microsoft.ML.Data.TextLoader;
 
 namespace Microsoft.ML.StaticPipe
 {
+    
     public static class DataLoadSaveOperationsExtensions
     {
-        /// <summary>
-        /// Configures a reader for text files.
-        /// </summary>
-        /// <typeparam name="TShape">The type shape parameter, which must be a valid-schema shape. As a practical
-        /// matter this is generally not explicitly defined from the user, but is instead inferred from the return
-        /// type of the <paramref name="func"/> where one takes an input <see cref="Context"/> and uses it to compose
-        /// a shape-type instance describing what the columns are and how to load them from the file.</typeparam>
-        /// <param name="catalog">The catalog.</param>
-        /// <param name="func">The delegate that describes what fields to read from the text file, as well as
-        /// describing their input type. The way in which it works is that the delegate is fed a <see cref="Context"/>,
-        /// and the user composes a shape type with <see cref="PipelineColumn"/> instances out of that <see cref="Context"/>.
-        /// The resulting data will have columns with the names corresponding to their names in the shape type.</param>
-        /// <param name="files">Input files.</param>
-        /// <param name="hasHeader">Data file has header with feature names.</param>
-        /// <param name="separator">Text field separator.</param>
-        /// <param name="allowQuoting">Whether the input -may include quoted values, which can contain separator
-        /// characters, colons, and distinguish empty values from missing values. When true, consecutive separators
-        /// denote a missing value and an empty value is denoted by <c>""</c>. When false, consecutive separators
-        /// denote an empty value.</param>
-        /// <param name="allowSparse">Whether the input may include sparse representations.</param>
-        /// <param name="trimWhitspace">Remove trailing whitespace from lines.</param>
-        /// <returns>A configured statically-typed reader for text files.</returns>
-        public static DataReader<IMultiStreamSource, TShape> CreateTextReader<[IsShape] TShape>(
+        /// <!-- Badly formed XML comment ignored for member "M:Microsoft.ML.StaticPipe.DataLoadSaveOperationsExtensions.CreateTextReader``1(Microsoft.ML.DataOperations,System.Func{Microsoft.ML.Data.TextLoader.Context,``0},Microsoft.ML.Data.IMultiStreamSource,System.Boolean,System.Char,System.Boolean,System.Boolean,System.Boolean)" -->
+                        public static DataReader<IMultiStreamSource, TShape> CreateTextReader<[IsShape] TShape>(
             this DataOperations catalog, Func<Context, TShape> func, IMultiStreamSource files = null,
             bool hasHeader = false, char separator = '\t', bool allowQuoting = true, bool allowSparse = true,
             bool trimWhitspace = false)
