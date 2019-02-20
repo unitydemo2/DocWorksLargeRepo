99f598189dbd9a8ad02b86f4d93922333806e8c7
CMSQATeam <cmsqa123@gmail.com>
2019-02-20 13:50:04 +0530

    DW CMS Message

--- src/Microsoft.ML.Data/EntryPoints/InputBuilder.cs
+++ src/Microsoft.ML.Data/EntryPoints/InputBuilder.cs
@@ -814,12 +814,14 @@ namespace Microsoft.ML.EntryPoints.JsonUtils
             public const string Min = "Min";
         }
 
-        /// <summary>
-        /// Obsolete Attribute specific field names.
-        /// </summary>
-        public static class Deprecated
+        ///     <summary>
+                ///     Obsolete Attribute specific field names.
+                ///     </summary>
+                        public static class Deprecated
         {
+            
             public static new string ToString() => "Deprecated";
+            
             public const string Message = "Message";
         }
 
