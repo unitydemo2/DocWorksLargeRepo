5a6013c7a3c1c6a60330e50e3c9a7105bfe1ccaa
CMSQATeam <cmsqa123@gmail.com>
2019-02-21 00:54:44 +0530

    DW CMS Message

--- src/Microsoft.ML.Data/Prediction/IPredictionTransformer.cs
+++ src/Microsoft.ML.Data/Prediction/IPredictionTransformer.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -8,14 +8,15 @@ using Microsoft.ML.Data;
 
 namespace Microsoft.ML
 {
-    /// <summary>
-    /// An interface for all the transformer that can transform data based on the <see cref="IPredictor"/> field.
-    /// The implemendations of this interface either have no feature column, or have more than one feature column, and cannot implement the
-    /// <see cref="ISingleFeaturePredictionTransformer{TModel}"/>, which most of the ML.Net tranformer implement.
-    /// </summary>
-    /// <typeparam name="TModel">The <see cref="IPredictor"/> or <see cref="ICalibrator"/> used for the data transformation.</typeparam>
-    public interface IPredictionTransformer<out TModel> : ITransformer
+    ///     <summary>
+        ///     An interface for all the transformer that can transform data based on the <see cref="IPredictor"/> field.
+        ///     The implemendations of this interface either have no feature column, or have more than one feature column, and cannot implement the
+        ///     <see cref="ISingleFeaturePredictionTransformer{TModel}"/>, which most of the ML.Net tranformer implement.
+        ///     </summary>
+        ///     <typeparam name="TModel">The <see cref="IPredictor"/> or <see cref="ICalibrator"/> used for the data transformation.</typeparam>
+            public interface IPredictionTransformer<out TModel> : ITransformer
     {
+        
         TModel Model { get; }
     }
 
