db94764ff1d8342cf43076916430a80c15647890
CMSQATeam <cmsqa123@gmail.com>
2019-02-20 21:04:23 +0530

    DW CMS Message

--- src/Microsoft.ML.FastTree/RandomForestClassification.cs
+++ src/Microsoft.ML.FastTree/RandomForestClassification.cs
@@ -115,14 +115,18 @@ namespace Microsoft.ML.Trainers.FastTree
             public sealed partial class FastForestClassification :
         RandomForestTrainerBase<FastForestClassification.Arguments, BinaryPredictionTransformer<IPredictorWithFeatureWeights<float>>, IPredictorWithFeatureWeights<float>>
     {
+        
         public sealed class Arguments : FastForestArgumentsBase
         {
+            
             [Argument(ArgumentType.AtMostOnce, HelpText = "Upper bound on absolute value of single tree output", ShortName = "mo")]
             public Double MaxTreeOutput = 100;
 
+            
             [Argument(ArgumentType.AtMostOnce, HelpText = "The calibrator kind to apply to the predictor. Specify null for no calibration", Visibility = ArgumentAttribute.VisibilityType.EntryPointsOnly)]
             public ICalibratorTrainerFactory Calibrator = new PlattCalibratorTrainerFactory();
 
+            
             [Argument(ArgumentType.AtMostOnce, HelpText = "The maximum number of examples to use when training the calibrator", Visibility = ArgumentAttribute.VisibilityType.EntryPointsOnly)]
             public int MaxCalibrationExamples = 1000000;
         }
