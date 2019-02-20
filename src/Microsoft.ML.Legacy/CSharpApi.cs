1dc443eb7a3c264b24e9cec998d2ada3d93cfbcd
CMSQATeam <cmsqa123@gmail.com>
2019-02-20 17:04:15 +0530

    DW CMS Message

--- src/Microsoft.ML.Legacy/CSharpApi.cs
+++ src/Microsoft.ML.Legacy/CSharpApi.cs
@@ -3812,60 +3812,60 @@ namespace Microsoft.ML
     namespace Legacy.Models
     {
 
-        /// <summary>
-        /// Combines a sequence of PredictorModels into a single model
-        /// </summary>
-        [Obsolete]
+        ///     <summary>
+                ///     Combines a sequence of PredictorModels into a single model
+                ///     </summary>
+                        [Obsolete]
         public sealed partial class OvaModelCombiner : Microsoft.ML.EntryPoints.CommonInputs.ITrainerInputWithWeight, Microsoft.ML.EntryPoints.CommonInputs.ITrainerInputWithLabel, Microsoft.ML.EntryPoints.CommonInputs.ITrainerInput, Microsoft.ML.Legacy.ILearningPipelineItem
         {
 
 
-            /// <summary>
-            /// Input models
-            /// </summary>
-            [Obsolete]
+            ///     <summary>
+                        ///     Input models
+                        ///     </summary>
+                                    [Obsolete]
             public ArrayVar<Microsoft.ML.EntryPoints.PredictorModel> ModelArray { get; set; } = new ArrayVar<Microsoft.ML.EntryPoints.PredictorModel>();
 
-            /// <summary>
-            /// Use probabilities from learners instead of raw values.
-            /// </summary>
-            [Obsolete]
+            ///     <summary>
+                        ///     Use probabilities from learners instead of raw values.
+                        ///     </summary>
+                                    [Obsolete]
             public bool UseProbabilities { get; set; } = true;
 
-            /// <summary>
-            /// Column to use for example weight
-            /// </summary>
-            [Obsolete]
+            ///     <summary>
+                        ///     Column to use for example weight
+                        ///     </summary>
+                                    [Obsolete]
             public Microsoft.ML.EntryPoints.Optional<string> WeightColumn { get; set; }
 
-            /// <summary>
-            /// Column to use for labels
-            /// </summary>
-            [Obsolete]
+            ///     <summary>
+                        ///     Column to use for labels
+                        ///     </summary>
+                                    [Obsolete]
             public string LabelColumn { get; set; } = "Label";
 
-            /// <summary>
-            /// The data to be used for training
-            /// </summary>
-            [Obsolete]
+            ///     <summary>
+                        ///     The data to be used for training
+                        ///     </summary>
+                                    [Obsolete]
             public Var<Microsoft.ML.Data.IDataView> TrainingData { get; set; } = new Var<Microsoft.ML.Data.IDataView>();
 
-            /// <summary>
-            /// Column to use for features
-            /// </summary>
-            [Obsolete]
+            ///     <summary>
+                        ///     Column to use for features
+                        ///     </summary>
+                                    [Obsolete]
             public string FeatureColumn { get; set; } = "Features";
 
-            /// <summary>
-            /// Normalize option for the feature column
-            /// </summary>
-            [Obsolete]
+            ///     <summary>
+                        ///     Normalize option for the feature column
+                        ///     </summary>
+                                    [Obsolete]
             public NormalizeOption NormalizeFeatures { get; set; } = NormalizeOption.Auto;
 
-            /// <summary>
-            /// Whether learner should cache input training data
-            /// </summary>
-            [Obsolete]
+            ///     <summary>
+                        ///     Whether learner should cache input training data
+                        ///     </summary>
+                                    [Obsolete]
             public CachingOptions Caching { get; set; } = CachingOptions.Auto;
 
 
@@ -3878,8 +3878,10 @@ namespace Microsoft.ML
                 public Var<Microsoft.ML.EntryPoints.PredictorModel> PredictorModel { get; set; } = new Var<Microsoft.ML.EntryPoints.PredictorModel>();
 
             }
+            
             [Obsolete]
             public Var<IDataView> GetInputData() => TrainingData;
+            
             
             [Obsolete]
             public ILearningPipelineStep ApplyStep(ILearningPipelineStep previousStep, Experiment experiment)
