131f19d54dc541dc87c15b60543889116cede86f
CMSQATeam <cmsqa123@gmail.com>
2019-02-20 13:40:58 +0530

    DW CMS Message

--- src/Microsoft.ML.Ensemble/Selector/IDiversityMeasure.cs
+++ src/Microsoft.ML.Ensemble/Selector/IDiversityMeasure.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -19,6 +19,7 @@ namespace Microsoft.ML.Ensemble.Selector
 
     public delegate void SignatureEnsembleDiversityMeasure();
 
+    
     public interface IBinaryDiversityMeasure : IDiversityMeasure<Single>
     { }
     public interface IRegressionDiversityMeasure : IDiversityMeasure<Single>
