3698a4402226fe0b4600abea0dd9fe6af6d3d926
CMSQATeam <cmsqa123@gmail.com>
2019-02-20 14:23:28 +0530

    DW CMS Message

--- src/Microsoft.ML.EntryPoints/DataViewReference.cs
+++ src/Microsoft.ML.EntryPoints/DataViewReference.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -11,6 +11,7 @@ using Microsoft.ML.EntryPoints;
 
 namespace Microsoft.ML.EntryPoints
 {
+    
     public class DataViewReference
     {
         public sealed class Input
@@ -25,6 +26,7 @@ namespace Microsoft.ML.EntryPoints
             public IDataView Data;
         }
 
+        
         [TlcModule.EntryPoint(Name = "Data.DataViewReference", Desc = "Pass dataview from memory to experiment")]
         public static Output ImportData(IHostEnvironment env, Input input)
         {
