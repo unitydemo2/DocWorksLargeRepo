4fa7a328bd835e22d41c26361140d82d1214c698
CMSQATeam <cmsqa123@gmail.com>
2019-02-20 21:13:28 +0530

    DW CMS Message

--- src/Microsoft.ML.FastTree/GamRegression.cs
+++ src/Microsoft.ML.FastTree/GamRegression.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -106,9 +106,11 @@ namespace Microsoft.ML.Trainers.FastTree
         }
     }
 
+    
     public class RegressionGamModelParameters : GamModelParametersBase
     {
         internal const string LoaderSignature = "RegressionGamPredictor";
+        
         public override PredictionKind PredictionKind => PredictionKind.Regression;
 
         internal RegressionGamModelParameters(IHostEnvironment env, int inputLength, Dataset trainset,
@@ -138,6 +140,7 @@ namespace Microsoft.ML.Trainers.FastTree
             return new RegressionGamModelParameters(env, ctx);
         }
 
+        
         private protected override void SaveCore(ModelSaveContext ctx)
         {
             Host.CheckValue(ctx, nameof(ctx));
