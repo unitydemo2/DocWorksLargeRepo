d8b929a5b9f1ff2de9dd4a72022aee3d40fbf9a0
CMSQATeam <cmsqa123@gmail.com>
2019-02-20 11:04:21 +0530

    DW CMS Message

--- src/Microsoft.ML.Core/Data/IEstimator.cs
+++ src/Microsoft.ML.Core/Data/IEstimator.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -26,10 +26,14 @@ namespace Microsoft.ML.Core.Data
 
         public struct Column
         {
+            
             public enum VectorKind
             {
+                
                 Scalar,
+                
                 Vector,
+                
                 VariableVector
             }
 
