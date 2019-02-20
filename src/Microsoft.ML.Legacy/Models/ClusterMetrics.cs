33dbf7a9e4097fd5d4f0bd379663c23d8a343857
CMSQATeam <cmsqa123@gmail.com>
2019-02-20 16:55:06 +0530

    DW CMS Message

--- src/Microsoft.ML.Legacy/Models/ClusterMetrics.cs
+++ src/Microsoft.ML.Legacy/Models/ClusterMetrics.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -9,10 +9,10 @@ using static Microsoft.ML.Data.MetricKinds;
 
 namespace Microsoft.ML.Legacy.Models
 {
-    /// <summary>
-    /// This class contains the overall metrics computed by cluster evaluators.
-    /// </summary>
-    [Obsolete]
+    ///     <summary>
+    ///     This class contains the overall metrics computed by cluster evaluators.
+    ///     </summary>
+        [Obsolete]
     public sealed class ClusterMetrics
     {
         private ClusterMetrics()
@@ -45,40 +45,40 @@ namespace Microsoft.ML.Legacy.Models
             return metrics;
         }
 
-        /// <summary>
-        /// Davies-Bouldin Index.
-        /// </summary>
-        /// <remarks>
-        /// DBI is a measure of the how much scatter is in the cluster and the cluster separation.
-        /// </remarks>
-        public double Dbi { get; private set; }
+        ///     <summary>
+                ///     Davies-Bouldin Index.
+                ///     </summary>
+                ///     <remarks>
+                ///     DBI is a measure of the how much scatter is in the cluster and the cluster separation.
+                ///     </remarks>
+                        public double Dbi { get; private set; }
 
-        /// <summary>
-        /// Normalized Mutual Information
-        /// </summary>
-        /// <remarks>
-        /// NMI is a measure of the mutual dependence between the true and predicted cluster labels for instances in the dataset.
-        /// NMI ranges between 0 and 1 where "0" indicates clustering is random and "1" indicates clustering is perfect w.r.t true labels.
-        /// </remarks>
-        public double Nmi { get; private set; }
+        ///     <summary>
+                ///     Normalized Mutual Information
+                ///     </summary>
+                ///     <remarks>
+                ///     NMI is a measure of the mutual dependence between the true and predicted cluster labels for instances in the dataset.
+                ///     NMI ranges between 0 and 1 where "0" indicates clustering is random and "1" indicates clustering is perfect w.r.t true labels.
+                ///     </remarks>
+                        public double Nmi { get; private set; }
 
-        /// <summary>
-        /// Average minimum score.
-        /// </summary>
-        /// <remarks>
-        /// AvgMinScore is the average squared-distance of examples from the respective cluster centroids.
-        /// It is defined as
-        /// AvgMinScore  = (1/m) * sum ((xi - c(xi))^2)
-        /// where m is the number of instances in the dataset.
-        /// xi is the i'th instance and c(xi) is the centriod of the predicted cluster for xi.
-        /// </remarks>
-        public double AvgMinScore { get; private set; }
+        ///     <summary>
+                ///     Average minimum score.
+                ///     </summary>
+                ///     <remarks>
+                ///     AvgMinScore is the average squared-distance of examples from the respective cluster centroids.
+                ///     It is defined as
+                ///     AvgMinScore  = (1/m) * sum ((xi - c(xi))^2)
+                ///     where m is the number of instances in the dataset.
+                ///     xi is the i'th instance and c(xi) is the centriod of the predicted cluster for xi.
+                ///     </remarks>
+                        public double AvgMinScore { get; private set; }
 
-        /// <summary>
-        /// For cross-validation, this is equal to "Fold N" for per-fold metric rows, "Overall" for the average metrics and "STD" for standard deviation.
-        /// For non-CV scenarios, this is equal to null
-        /// </summary>
-        public string RowTag { get; private set; }
+        ///     <summary>
+                ///     For cross-validation, this is equal to "Fold N" for per-fold metric rows, "Overall" for the average metrics and "STD" for standard deviation.
+                ///     For non-CV scenarios, this is equal to null
+                ///     </summary>
+                        public string RowTag { get; private set; }
 
         /// <summary>
         /// This class contains the public fields necessary to deserialize from IDataView.
